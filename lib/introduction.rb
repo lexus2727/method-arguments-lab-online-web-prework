f4cd73e4034e43e9f791a83448501e8386c7b877
compassionate-scope-9333 <lexus2424@gmail.com>
2019-04-10 17:27:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,6 @@
-def introduction(name)
-  puts "Hi, my name is #{name}."
+def introduction_with_languge(name, language)
+  puts "Hi, my name is #{name}." "I am a #{language} programmer."
 end
+
+introducton_with_language("Candice", "JAVA")
+
